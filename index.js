f6a02682d9d2125662996ed5a835569eedcbab03
Thorsten Lorenz <thlorenz@gmx.de>
2013-08-27 22:04:56 -0400

    initial index

---
+++ index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// Readme Example:
+// foo/node_modules/bar is src
+// bar                  is tgt 
+var go = module.exports = function (root, src, tgt, cb) {
+
+  // 1. Create pseudo package (bar@)
+  // 2.  
+  
+};
+
+
+
+
