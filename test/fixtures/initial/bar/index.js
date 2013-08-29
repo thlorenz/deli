74d74229b20eef6b3807beb66865436a126db26c
Thorsten Lorenz <thlorenz@gmx.de>
2013-08-29 10:57:15 -0400

    adding non-common dependency

--- test/fixtures/initial/bar/index.js
+++ test/fixtures/initial/bar/index.js
@@ -2,8 +2,9 @@
 
 var pack = require('./package.json');
 var common = require('foobar-common');
+var notcommon = require('foobar-not-common');
 
 var go = module.exports = function () {
   var me = pack.name + '@' + pack.version;
-  return '[ me: ' + me + ' common: ' + common() + ' ] ';
+  return '[ me: ' + me + ' common: ' + common() + ' not-common: ' + notcommon() + ' ] ';
 };
